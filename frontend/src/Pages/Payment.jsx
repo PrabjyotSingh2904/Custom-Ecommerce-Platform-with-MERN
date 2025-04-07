85ab3926380844a7d170694589a5d9ea847c45e6
PrabjyotSingh2904 <princenagoka2904@gmail.com>
2025-04-07 20:24:11 +0530

    Add files via upload

---
+++ frontend/src/Pages/Payment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PaymentDetails from '../Components/Payment/PaymentDetails';
+
+const Payment = () => {
+  return (
+    <div>
+      <PaymentDetails />
+    </div>
+  );
+};
+
+export default Payment;
